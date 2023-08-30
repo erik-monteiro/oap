8772ef447f1e5424c3737f5988983855f8ee8ae8
Erik Monteiro Ribeiro <91336496+erik-monteiro@users.noreply.github.com>
2023-08-30 10:51:40 -0300

    Update App.java

---
+++ App.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    System.out.println("Programa Ackermann");
+	    System.out.println("“Componentes: <Gabriel Verdi, Erik Monteiro, Henrique>");
+	    
+	    while (true) {
+	       	String s = lerValoresEPrintar();
+	       	System.out.println(s);
+	    }
+	}
+	
+	public static String lerValoresEPrintar() {
+        Scanner sc = new Scanner(System.in);
+       	
+       	System.out.println("Digite os parâmetros m e n para calcular A(m, n) ou -1 para abortar a execução");
+	    int m = sc.nextInt();
+	    if (m < 0) System.exit(0);
+	    int n = sc.nextInt();
+	    if (n < 0) System.exit(0);
+	
+	    return "A(" + m + ", " + n + ") = " + ackermann(m, n); 
+	}
+	
+	public static int ackermann(int m, int n) {
+	    int result = 0;
+	    
+	    if (m == 0) result = n + 1;
+	    
+	    if (m > 0 && n == 0) result = ackermann(m - 1, 1);
+	    
+	    if (m > 0 && n > 0) result = ackermann(m - 1, ackermann(m, n - 1));
+	    
+	    return result;
+	}
+}
